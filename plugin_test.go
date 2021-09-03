a4841760e8cf82431d6beaea80faf5887cebcb1e
Max <shadow.iqka@qq.com>
2021-09-03 08:58:08 +0800

    - plugin test

--- plugin_test.go
+++ plugin_test.go
@@ -1,18 +1,15 @@
 package gou
 
 import (
-	"path"
 	"testing"
-
-	"github.com/yaoapp/kun/utils"
 )
 
 func TestLoadPlugin(t *testing.T) {
-	cmd := path.Join(TestPLGRoot, "user")
-	p := LoadPlugin(cmd, "user")
-	defer p.Client.Kill()
+	// cmd := path.Join(TestPLGRoot, "user")
+	// p := LoadPlugin(cmd, "user")
+	// defer p.Client.Kill()
 
-	mod := SelectPluginModel("user")
-	res, err := mod.Exec("login", "13111021983", "#991832")
-	utils.Dump(res.MustMap(), err)
+	// mod := SelectPluginModel("user")
+	// res, err := mod.Exec("login", "13111021983", "#991832")
+	// utils.Dump(res.MustMap(), err)
 }
