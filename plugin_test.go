86a03ac6cbecd0efae0244dc99072adb3feb16a9
Max <shadow.iqka@qq.com>
2021-09-05 10:16:09 +0800

    删除无用数据

--- plugin_test.go
+++ plugin_test.go
@@ -4,16 +4,18 @@ import (
 	"path"
 	"testing"
 
-	"github.com/yaoapp/kun/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadPlugin(t *testing.T) {
 	cmd := path.Join(TestPLGRoot, "user")
-	utils.Dump(cmd)
 	p := LoadPlugin(cmd, "user")
 	defer p.Client.Kill()
 
 	mod := SelectPluginModel("user")
 	res, err := mod.Exec("login", "13111021983", "#991832")
-	utils.Dump(res.MustMap(), err)
+	assert.Equal(t, res.MustMap().Dot().Get("name"), "login")
+	assert.Equal(t, res.MustMap().Dot().Get("args.0"), "13111021983")
+	assert.Equal(t, res.MustMap().Dot().Get("args.1"), "#991832")
+	assert.Nil(t, err)
 }
