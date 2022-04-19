27608094ab7f16ac54bb5eff5a7f57967d233495
影浅 <soyz@qq.com>
2022-04-19 20:02:24 +0800

    style: Remove excess alias

--- runtime/yao/yao.go
+++ runtime/yao/yao.go
@@ -15,24 +15,23 @@ import (
 	"github.com/yaoapp/kun/log"
 	"github.com/yaoapp/kun/utils"
 	"rogchap.com/v8go"
-	v8 "rogchap.com/v8go"
 )
 
-// New create a pure ES6 engine (v8)
+// New create a pure ES6 engine (v8go)
 func New(numOfContexts int) *Yao {
-	iso := v8.NewIsolate()
-	global := v8.NewObjectTemplate(iso)
+	iso := v8go.NewIsolate()
+	global := v8go.NewObjectTemplate(iso)
 
 	yao := &Yao{
 		iso:             iso,
 		template:        global,
 		scripts:         map[string]script{},
-		objectTemplates: map[string]*v8.ObjectTemplate{},
+		objectTemplates: map[string]*v8go.ObjectTemplate{},
 		numOfContexts:   numOfContexts,
 	}
 
-	yao.template.Set("fetch", v8.NewFunctionTemplate(yao.iso, yao.jsFetch))
-	yao.AddFunctionTemplates(map[string]*v8.FunctionTemplate{
+	yao.template.Set("fetch", v8go.NewFunctionTemplate(yao.iso, yao.jsFetch))
+	yao.AddFunctionTemplates(map[string]*v8go.FunctionTemplate{
 		"Exception": objects.NewException().ExportFunction(yao.iso),
 		"WebSocket": objects.NewWebSocket().ExportFunction(yao.iso),
 		"Store":     objects.NewStore().ExportFunction(yao.iso),
@@ -43,11 +42,11 @@ func New(numOfContexts int) *Yao {
 // Init initialize (for next version )
 func (yao *Yao) Init() error {
 	// yao.contexts = NewPool(yao.numOfContexts)
-	// yao.ctx = v8.NewContext(yao.iso, yao.template)
+	// yao.ctx = v8go.NewContext(yao.iso, yao.template)
 	// for i := 0; i < yao.numOfContexts; i++ {
-	// 	yao.contexts.Push(v8.NewContext(yao.iso, yao.template))
+	// 	yao.contexts.Push(v8go.NewContext(yao.iso, yao.template))
 	// }
-	// go yao.contexts.Prepare(200, func() *v8.Context { return v8.NewContext(yao.iso, yao.template) })
+	// go yao.contexts.Prepare(200, func() *v8go.Context { return v8go.NewContext(yao.iso, yao.template) })
 	return nil
 }
 
@@ -79,13 +78,13 @@ func (yao *Yao) LoadReader(reader io.Reader, name string, filename ...string) er
 
 	// Compile
 	code := string(source)
-	// compiled, err := yao.iso.CompileUnboundScript(code, scriptfile, v8.CompileOptions{}) // compile script to get cached data
+	// compiled, err := yao.iso.CompileUnboundScript(code, scriptfile, v8go.CompileOptions{}) // compile script to get cached data
 	// if err != nil {
 	// 	return err
 	// }
 
 	// opt
-	ctx := v8.NewContext(yao.iso, yao.template)
+	ctx := v8go.NewContext(yao.iso, yao.template)
 	ctx.RunScript(code, scriptfile)
 
 	yao.scripts[name] = script{
@@ -107,7 +106,7 @@ func (yao *Yao) Call(data map[string]interface{}, name string, method string, ar
 
 	var err error
 	v8ctx := script.ctx
-	// v8ctx, err := yao.contexts.Make(func() *v8.Context { return v8.NewContext(yao.iso, yao.template) })
+	// v8ctx, err := yao.contexts.Make(func() *v8go.Context { return v8go.NewContext(yao.iso, yao.template) })
 	// if err != nil {
 	// 	return nil, err
 	// }
@@ -158,7 +157,7 @@ func (yao *Yao) Call(data map[string]interface{}, name string, method string, ar
 			log.Error("function execute error. %s.%s %s", name, method, err.Error())
 			return nil, err
 		}
-		for promise.State() == v8.Pending {
+		for promise.State() == v8go.Pending {
 			continue
 		}
 		value = promise.Result()
@@ -180,8 +179,8 @@ func (yao *Yao) AddFunction(name string, fn func(global map[string]interface{}, 
 	return nil
 }
 
-func (yao *Yao) goFunTemplate(fn func(global map[string]interface{}, sid string, args ...interface{}) interface{}) *v8.FunctionTemplate {
-	return v8.NewFunctionTemplate(yao.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+func (yao *Yao) goFunTemplate(fn func(global map[string]interface{}, sid string, args ...interface{}) interface{}) *v8go.FunctionTemplate {
+	return v8go.NewFunctionTemplate(yao.iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
 		global := map[string]interface{}{}
 		jsGlobal, _ := info.Context().Global().Get("__yao_global")
 		anyGlobal, _ := ToInterface(jsGlobal)
@@ -197,7 +196,7 @@ func (yao *Yao) goFunTemplate(fn func(global map[string]interface{}, sid string,
 
 // AddObject add a global object
 func (yao *Yao) AddObject(name string, methods map[string]func(global map[string]interface{}, sid string, args ...interface{}) interface{}) error {
-	object := v8.NewObjectTemplate(yao.iso)
+	object := v8go.NewObjectTemplate(yao.iso)
 	for method, fn := range methods {
 		jsFun := yao.goFunTemplate(fn)
 		object.Set(method, jsFun)
@@ -214,27 +213,27 @@ func (yao *Yao) AddFunctionTemplates(tmpls map[string]*v8go.FunctionTemplate) er
 	return nil
 }
 
-func (yao *Yao) jsLog(info *v8.FunctionCallbackInfo) *v8.Value {
+func (yao *Yao) jsLog(info *v8go.FunctionCallbackInfo) *v8go.Value {
 	values := valuesToArray(info.Args())
 	utils.Dump(values)
 	return nil
 }
 
-func (yao *Yao) jsFetch(info *v8.FunctionCallbackInfo) *v8.Value {
+func (yao *Yao) jsFetch(info *v8go.FunctionCallbackInfo) *v8go.Value {
 	args := info.Args()
 	url := args[0].String()
-	resolver, _ := v8.NewPromiseResolver(info.Context())
+	resolver, _ := v8go.NewPromiseResolver(info.Context())
 	go func() {
 		res, _ := http.Get(url)
 		body, _ := ioutil.ReadAll(res.Body)
-		val, _ := v8.NewValue(yao.iso, string(body))
+		val, _ := v8go.NewValue(yao.iso, string(body))
 		resolver.Resolve(val)
 	}()
 	return resolver.GetPromise().Value
 }
 
-// ToInterface Convert *v8.Value to Interface
-func ToInterface(value *v8.Value) (interface{}, error) {
+// ToInterface Convert *v8go.Value to Interface
+func ToInterface(value *v8go.Value) (interface{}, error) {
 
 	if value == nil {
 		return nil, nil
@@ -258,8 +257,8 @@ func ToInterface(value *v8.Value) (interface{}, error) {
 	return v, nil
 }
 
-// MustAnyToValue Convert any to *v8.Value
-func MustAnyToValue(ctx *v8.Context, value interface{}) *v8.Value {
+// MustAnyToValue Convert any to *v8go.Value
+func MustAnyToValue(ctx *v8go.Context, value interface{}) *v8go.Value {
 	v, err := AnyToValue(ctx, value)
 	if err != nil {
 		exception.Err(err, 500).Throw()
@@ -267,15 +266,15 @@ func MustAnyToValue(ctx *v8.Context, value interface{}) *v8.Value {
 	return v
 }
 
-// AnyToValue Convert data to *v8.Value
-func AnyToValue(ctx *v8.Context, value interface{}) (*v8.Value, error) {
+// AnyToValue Convert data to *v8go.Value
+func AnyToValue(ctx *v8go.Context, value interface{}) (*v8go.Value, error) {
 
 	switch value.(type) {
 	case []byte:
 		// Todo: []byte to Uint8Array
-		return v8.NewValue(ctx.Isolate(), string(value.([]byte)))
+		return v8go.NewValue(ctx.Isolate(), string(value.([]byte)))
 	case string, int32, uint32, int64, uint64, bool, float64, *big.Int:
-		return v8.NewValue(ctx.Isolate(), value)
+		return v8go.NewValue(ctx.Isolate(), value)
 	}
 
 	v, err := jsoniter.Marshal(value)
@@ -283,21 +282,21 @@ func AnyToValue(ctx *v8.Context, value interface{}) (*v8.Value, error) {
 		log.Error("AnyToValue error: %s", err)
 	}
 
-	return v8.JSONParse(ctx, string(v))
+	return v8go.JSONParse(ctx, string(v))
 }
 
-func interfaceToValue(ctx *v8.Context, value interface{}) *v8.Value {
-	var valuer *v8.Value
+func interfaceToValue(ctx *v8go.Context, value interface{}) *v8go.Value {
+	var valuer *v8go.Value
 	if value == nil {
-		valuer, _ = v8.NewValue(ctx.Isolate(), value)
+		valuer, _ = v8go.NewValue(ctx.Isolate(), value)
 		return valuer
 	}
 	v, _ := jsoniter.Marshal(value)
-	valuer, _ = v8.JSONParse(ctx, string(v))
+	valuer, _ = v8go.JSONParse(ctx, string(v))
 	return valuer
 }
 
-func valuesToArray(values []*v8.Value) []interface{} {
+func valuesToArray(values []*v8go.Value) []interface{} {
 	res := []interface{}{}
 	for i := range values {
 		var v interface{} = nil
@@ -313,9 +312,9 @@ func valuesToArray(values []*v8.Value) []interface{} {
 	return res
 }
 
-// ToValuers Convert any to []v8.Valuer
-func ToValuers(ctx *v8.Context, values []interface{}) ([]v8.Valuer, error) {
-	res := []v8.Valuer{}
+// ToValuers Convert any to []v8go.Valuer
+func ToValuers(ctx *v8go.Context, values []interface{}) ([]v8go.Valuer, error) {
+	res := []v8go.Valuer{}
 	if ctx == nil {
 		return res, fmt.Errorf("Context is nil")
 	}
@@ -324,7 +323,7 @@ func ToValuers(ctx *v8.Context, values []interface{}) ([]v8.Valuer, error) {
 		value, err := AnyToValue(ctx, values[i])
 		if err != nil {
 			log.Error("AnyToValue error: %s", err)
-			value, _ = v8.NewValue(ctx.Isolate(), nil)
+			value, _ = v8go.NewValue(ctx.Isolate(), nil)
 		}
 		res = append(res, value)
 	}
--- runtime/yao/yao_test.go
+++ runtime/yao/yao_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	v8 "rogchap.com/v8go"
+	"rogchap.com/v8go"
 )
 
 func TestMustAnyToValue(t *testing.T) {
-	ctx := v8.NewContext()
+	ctx := v8go.NewContext()
 	v := MustAnyToValue(ctx, 0.618)
 	assert.True(t, v.IsNumber())
 }
