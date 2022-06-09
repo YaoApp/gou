cb0b221c96a6900d286a142fd3ecb11587a63a8b
Max <shadow.iqka@qq.com>
2022-06-09 15:16:48 +0800

    [add] request CloseIdleConnections

--- dns/dsn_test.go
+++ dns/dsn_test.go
@@ -8,31 +8,34 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yaoapp/kun/utils"
 )
 
 func TestLookupIP(t *testing.T) {
-	_, err := LookupIP("localhost", true)
+	_, err := LookupIP("github.com", true)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestLookupIPCaches(t *testing.T) {
-	ips, err := LookupIP("localhost", true)
+	ips, err := LookupIP("github.com", true)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t, ips, caches["localhost_true"])
+	assert.Equal(t, ips, caches["github.com_true"])
+	utils.Dump(ips)
 
-	_, has := caches["localhost_false"]
+	_, has := caches["github.com_false"]
 	assert.Equal(t, false, has)
 
-	ips, err = LookupIP("localhost", false)
+	ips, err = LookupIP("github.com", false)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, ips, caches["localhost_false"])
+	utils.Dump(ips)
+	assert.Equal(t, ips, caches["github.com_false"])
 
 }
 
