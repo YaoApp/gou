eebc8a23c94c231c75c34ac87c640e5320aa582f
Max <shadow.iqka@qq.com>
2022-09-17 16:13:21 +0800

    [add] mongodb connector

---
+++ connector/mongo/mongo.go
@@ -0,0 +1,139 @@
+package m
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/yaoapp/gou/helper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Connector the ConnectorDB struct
+type Connector struct {
+	Name    string        `json:"-"`
+	Version string        `json:"version,omitempty"`
+	Options Options       `json:"options"`
+	Client  *mongo.Client `json:"-"`
+}
+
+// Options the connetion options
+type Options struct {
+	DB      string                 `json:"db"`
+	Timeout int                    `json:"timeout,omitempty"`
+	Hosts   []Host                 `json:"hosts"`
+	Params  map[string]interface{} `json:"params"`
+	dsn     string
+}
+
+// Host the connection host
+type Host struct {
+	Host string `json:"host,omitempty"`
+	Port string `json:"port,omitempty"`
+	User string `json:"user,omitempty"`
+	Pass string `json:"pass,omitempty"`
+}
+
+// Register the connections from dsl
+func (m *Connector) Register(name string, dsl []byte) error {
+	err := jsoniter.Unmarshal(dsl, m)
+	if err != nil {
+		return err
+	}
+
+	err = m.setDefaults()
+	if err != nil {
+		return err
+	}
+
+	m.Name = name
+	return m.makeConnection()
+}
+
+// Is the connections from dsl
+func (m *Connector) Is(typ int) bool {
+	return 3 == typ
+}
+
+func (m *Connector) makeConnection() error {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(m.Options.dsn))
+	if err != nil {
+		return err
+	}
+
+	client.Database(m.Options.DB)
+	m.Client = client
+	return nil
+}
+
+func (m *Connector) setDefaults() error {
+	m.Options.DB = helper.EnvString(m.Options.DB)
+	m.Options.Timeout = helper.EnvInt(m.Options.Timeout, 5)
+	if m.Options.Timeout == 0 {
+		m.Options.Timeout = 5
+	}
+
+	for i := range m.Options.Hosts {
+		m.Options.Hosts[i].Host = helper.EnvString(m.Options.Hosts[i].Host)
+		m.Options.Hosts[i].Pass = helper.EnvString(m.Options.Hosts[i].Pass)
+		m.Options.Hosts[i].User = helper.EnvString(m.Options.Hosts[i].User)
+		m.Options.Hosts[i].Port = helper.EnvString(m.Options.Hosts[i].Port)
+
+		dsn, err := m.getDSN()
+		if err != nil {
+			return err
+		}
+		m.Options.dsn = dsn
+	}
+	return nil
+}
+
+// getDSN get the DSN
+func (m *Connector) getDSN() (string, error) {
+
+	if m.Options.DB == "" {
+		return "", fmt.Errorf("options.db is required")
+	}
+
+	if len(m.Options.Hosts) == 0 {
+		return "", fmt.Errorf("options.hosts is required")
+	}
+
+	hosts := []string{}
+	for i := range m.Options.Hosts {
+		host := m.Options.Hosts[i]
+		if host.Host == "" {
+			return "", fmt.Errorf("hosts.%d.host is required", i)
+		}
+
+		if host.Port == "" {
+			host.Port = "27017"
+		}
+
+		if host.User == "" {
+			return "", fmt.Errorf("hosts.%d.user is required", i)
+		}
+
+		if host.Pass == "" {
+			return "", fmt.Errorf("hosts.%d.pass is required", i)
+		}
+
+		hosts = append(hosts, fmt.Sprintf("%s:%s@%s:%s", host.User, host.Pass, host.Host, host.Port))
+	}
+
+	params := []string{}
+	if m.Options.Params != nil {
+		for name, value := range m.Options.Params {
+			params = append(params, fmt.Sprintf("%s=%v", name, value))
+		}
+	}
+
+	dsn := fmt.Sprintf("mongodb://%s/", strings.Join(hosts, ","))
+	if len(params) > 0 {
+		dsn = dsn + "?" + strings.Join(params, "&")
+	}
+
+	return dsn, nil
+}
--- go.mod
+++ go.mod
@@ -20,9 +20,22 @@ require (
 	github.com/tidwall/buntdb v1.2.9
 	github.com/yaoapp/kun v0.9.0
 	github.com/yaoapp/xun v0.0.0-00010101000000-000000000000
-	golang.org/x/crypto v0.0.0-20220208050332-20e1d8d225ab
-	gopkg.in/yaml.v3 v3.0.0
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+	gopkg.in/yaml.v3 v3.0.1
 	rogchap.com/v8go v0.7.0
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.10.2 
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
 
 require (
