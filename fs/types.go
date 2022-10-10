5c92bdb198f2d4b12f1236f348830b3e295a7b65
Max <shadow.iqka@qq.com>
2022-10-10 21:11:56 +0800

    [add] fs & go upgrade to v1.9.2

---
+++ fs/types.go
@@ -0,0 +1,32 @@
+package fs
+
+import "time"
+
+// FileSystem the filesystem io interface
+type FileSystem interface {
+	ReadFile(file string) ([]byte, error)
+	WriteFile(file string, data []byte, pterm int) (int, error)
+
+	ReadDir(dir string, recursive bool) ([]string, error)
+	Mkdir(dir string, pterm int) error
+	MkdirAll(dir string, pterm int) error
+	MkdirTemp(dir string, pattern string) (string, error)
+
+	Remove(name string) error
+	RemoveAll(name string) error
+
+	Exists(name string) (bool, error)
+	Size(name string) (int, error)
+	Mode(name string) (int, error)
+	ModTime(name string) (time.Time, error)
+
+	Chmod(name string, mode int) error
+	IsDir(name string) bool
+	IsFile(name string) bool
+	IsLink(name string) bool
+
+	Move(oldpath string, newpath string) error
+	Copy(src string, dest string) error
+
+	MimeType(name string) (string, error)
+}
