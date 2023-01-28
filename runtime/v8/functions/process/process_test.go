7f114a6161fe47cc55cf79ed453b982164ca5656
Max <shadow.iqka@qq.com>
2023-01-28 20:28:07 +0800

    [add]  jsAPI  Process, Studio tests

---
+++ runtime/v8/functions/process/process_test.go
@@ -0,0 +1,125 @@
+package process
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yaoapp/gou/process"
+	"github.com/yaoapp/gou/runtime/v8/bridge"
+	"rogchap.com/v8go"
+)
+
+func TestProcess(t *testing.T) {
+
+	ctx := prepare(t, false, "", nil)
+	defer close(ctx)
+
+	jsRes, err := ctx.RunScript(`
+		const test = () => {
+			const result = Process("unit.test.process", "foo", 99, 0.618);
+			return {
+				...result,
+				__yao_global:__yao_global,
+				__yao_sid: __yao_sid,
+				__YAO_SU_ROOT: __YAO_SU_ROOT,
+			}
+		}
+		test()
+	`, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	goRes, err := bridge.GoValue(jsRes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, ok := goRes.(map[string]interface{})
+	if !ok {
+		t.Fatal("result error")
+	}
+
+	assert.Equal(t, res["Sid"], res["__yao_sid"])
+	assert.Equal(t, nil, res["__yao_global"])
+	assert.Equal(t, false, res["__YAO_SU_ROOT"])
+	assert.Equal(t, "unit.test.process", res["Name"])
+	assert.Equal(t, []interface{}{"foo", float64(99), 0.618}, res["Args"])
+}
+
+func TestProcessWithData(t *testing.T) {
+
+	ctx := prepare(t, true, "SID-0101", map[string]interface{}{"hello": "world"})
+	defer close(ctx)
+
+	jsRes, err := ctx.RunScript(`
+		const test = () => {
+			const result = Process("unit.test.process", "foo", 99, 0.618);
+			return {
+				...result,
+				__yao_global:__yao_global,
+				__yao_sid: __yao_sid,
+				__YAO_SU_ROOT: __YAO_SU_ROOT,
+			}
+		}
+		test()
+	`, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	goRes, err := bridge.GoValue(jsRes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, ok := goRes.(map[string]interface{})
+	if !ok {
+		t.Fatal("result error")
+	}
+
+	assert.Equal(t, res["Sid"], res["__yao_sid"])
+	assert.Equal(t, res["Global"], res["__yao_global"])
+	assert.Equal(t, "SID-0101", res["__yao_sid"])
+	assert.Equal(t, map[string]interface{}{"hello": "world"}, res["__yao_global"])
+	assert.Equal(t, true, res["__YAO_SU_ROOT"])
+	assert.Equal(t, "unit.test.process", res["Name"])
+	assert.Equal(t, []interface{}{"foo", float64(99), 0.618}, res["Args"])
+}
+
+func close(ctx *v8go.Context) {
+	ctx.Isolate().Dispose()
+}
+
+func prepare(t *testing.T, root bool, sid string, global map[string]interface{}) *v8go.Context {
+
+	iso := v8go.NewIsolate()
+
+	template := v8go.NewObjectTemplate(iso)
+	template.Set("Process", ExportFunction(iso))
+
+	ctx := v8go.NewContext(iso, template)
+
+	var err error
+	jsGlobal := v8go.Undefined(ctx.Isolate())
+	jsGlobal, err = bridge.JsValue(ctx, global)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = ctx.Global().Set("__YAO_SU_ROOT", root); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = ctx.Global().Set("__yao_global", jsGlobal); err != nil {
+		t.Fatal(err)
+	}
+	if err = ctx.Global().Set("__yao_sid", sid); err != nil {
+		t.Fatal(err)
+	}
+
+	process.Register("unit.test.process", func(process *process.Process) interface{} {
+		return process
+	})
+	return ctx
+}
