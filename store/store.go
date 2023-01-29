daabb0415e629d98d00f2db5f50f838db7f838ff
Max <shadow.iqka@qq.com>
2023-01-29 16:16:18 +0800

    [add] migrate store

--- store/store.go
+++ store/store.go
@@ -2,16 +2,66 @@ package store
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/yaoapp/gou/application"
 	"github.com/yaoapp/gou/connector"
 	"github.com/yaoapp/gou/helper"
 	"github.com/yaoapp/gou/store/lru"
 	"github.com/yaoapp/gou/store/mongo"
 	"github.com/yaoapp/gou/store/redis"
+	"github.com/yaoapp/kun/exception"
 )
 
 // Pools LRU pools
 var Pools = map[string]Store{}
+
+// Load load kv store
+func Load(file string, name string) (Store, error) {
+
+	data, err := application.App.Read(file)
+	if err != nil {
+		return nil, err
+	}
+
+	inst := Instance{}
+	err = application.Parse(file, data, &inst)
+	if err != nil {
+		return nil, err
+	}
+
+	typ := strings.ToLower(inst.Type)
+	if typ == "lru" {
+		stor, err := New(nil, inst.Option)
+		if err != nil {
+			return nil, err
+		}
+		Pools[name] = stor
+		return Pools[name], nil
+	}
+
+	connector, has := connector.Connectors[inst.Connector]
+	if !has {
+		return nil, fmt.Errorf("Store %s Connector:%s was not loaded", name, inst.Connector)
+	}
+
+	stor, err := New(connector, inst.Option)
+	if err != nil {
+		return nil, err
+	}
+
+	Pools[name] = stor
+	return Pools[name], nil
+}
+
+// Select Select loaded kv store
+func Select(name string) Store {
+	store, has := Pools[name]
+	if !has {
+		exception.New("Store:%s does not load", 500, name).Throw()
+	}
+	return store
+}
 
 // New create a store via connector
 func New(c connector.Connector, option Option) (Store, error) {
