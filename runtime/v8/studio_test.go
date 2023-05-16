ea461f6082337ffd98986cfb9cab3e4a83c0695d
Max <shadow.iqka@qq.com>
2023-05-16 21:22:34 +0800

    [fix] v8 JSAPI Studio

--- runtime/v8/studio_test.go
+++ runtime/v8/studio_test.go
@@ -1,24 +1,27 @@
-package studio
+package v8
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/yaoapp/gou/process"
 	"github.com/yaoapp/gou/runtime/v8/bridge"
+	"github.com/yaoapp/gou/runtime/v8/functions/studio"
 	"rogchap.com/v8go"
 )
 
 func TestStudio(t *testing.T) {
+	prepare(t)
 
-	ctx := prepare(t, true, "", nil)
-	defer close(ctx)
+	ctx := prepareStudio(t, true, "", nil)
+	defer closeStudio(ctx)
 
 	jsRes, err := ctx.RunScript(`
 		const test = () => {
-			const result = Studio("unit.test.process", "foo", 99, 0.618);
+			const result = Studio("studio.runtime.basic.Hello", "foo", 99, 0.618);
+			const result2 = Studio("runtime.basic.Hello", "foo", 99, 0.618);
 			return {
-				...result,
+				result: result,
+				result2: result2,
 				__yao_global:__yao_data["DATA"],
 				__yao_sid:__yao_data["SID"],
 				__YAO_SU_ROOT:__yao_data["ROOT"],
@@ -40,23 +43,24 @@ func TestStudio(t *testing.T) {
 		t.Fatal("result error")
 	}
 
-	assert.Equal(t, res["Sid"], res["__yao_sid"])
+	assert.Equal(t, "", res["__yao_sid"])
 	assert.Equal(t, nil, res["__yao_global"])
 	assert.Equal(t, true, res["__YAO_SU_ROOT"])
-	assert.Equal(t, "unit.test.process", res["Name"])
-	assert.Equal(t, []interface{}{"foo", float64(99), 0.618}, res["Args"])
+	assert.Equal(t, "foo", res["result"])
+	assert.Equal(t, "foo", res["result2"])
 }
 
 func TestStudioWithData(t *testing.T) {
+	prepare(t)
 
-	ctx := prepare(t, true, "SID-0101", map[string]interface{}{"hello": "world"})
-	defer close(ctx)
+	ctx := prepareStudio(t, true, "SID-0101", map[string]interface{}{"hello": "world"})
+	defer closeStudio(ctx)
 
 	jsRes, err := ctx.RunScript(`
 		const test = () => {
-			const result = Studio("unit.test.process", "foo", 99, 0.618);
+			const result = Studio("studio.runtime.basic.Hello", "foo", 99, 0.618);
 			return {
-				...result,
+				result: result,
 				__yao_global:__yao_data["DATA"],
 				__yao_sid:__yao_data["SID"],
 				__YAO_SU_ROOT:__yao_data["ROOT"],
@@ -78,23 +82,22 @@ func TestStudioWithData(t *testing.T) {
 		t.Fatal("result error")
 	}
 
-	assert.Equal(t, res["Sid"], res["__yao_sid"])
-	assert.Equal(t, res["Global"], res["__yao_global"])
+	assert.Equal(t, "SID-0101", res["__yao_sid"])
 	assert.Equal(t, "SID-0101", res["__yao_sid"])
 	assert.Equal(t, map[string]interface{}{"hello": "world"}, res["__yao_global"])
 	assert.Equal(t, true, res["__YAO_SU_ROOT"])
-	assert.Equal(t, "unit.test.process", res["Name"])
-	assert.Equal(t, []interface{}{"foo", float64(99), 0.618}, res["Args"])
+	assert.Equal(t, "foo", res["result"])
 }
 
 func TestStudioNotRoot(t *testing.T) {
+	prepare(t)
 
-	ctx := prepare(t, false, "", nil)
-	defer close(ctx)
+	ctx := prepareStudio(t, false, "", nil)
+	defer closeStudio(ctx)
 
 	_, err := ctx.RunScript(`
 		const test = () => {
-			const result = Studio("unit.test.process", "foo", 99, 0.618);
+			const result = Studio("studio.runtime.basic.Hello", "foo", 99, 0.618);
 			return {
 				...result,
 				__yao_global:__yao_data["DATA"],
@@ -108,20 +111,18 @@ func TestStudioNotRoot(t *testing.T) {
 	assert.Equal(t, "Error: function is not allowed", err.Error())
 }
 
-func close(ctx *v8go.Context) {
+func closeStudio(ctx *v8go.Context) {
 	ctx.Isolate().Dispose()
 }
 
-func prepare(t *testing.T, root bool, sid string, global map[string]interface{}) *v8go.Context {
+func prepareStudio(t *testing.T, root bool, sid string, global map[string]interface{}) *v8go.Context {
 
 	iso := v8go.NewIsolate()
 
 	template := v8go.NewObjectTemplate(iso)
-	template.Set("Studio", ExportFunction(iso))
+	template.Set("Studio", studio.ExportFunction(iso))
 
 	ctx := v8go.NewContext(iso, template)
-
-	var err error
 	goData := map[string]interface{}{
 		"SID":  sid,
 		"ROOT": root,
@@ -137,8 +138,5 @@ func prepare(t *testing.T, root bool, sid string, global map[string]interface{})
 		t.Fatal(err)
 	}
 
-	process.Register("unit.test.process", func(process *process.Process) interface{} {
-		return process
-	})
 	return ctx
 }
