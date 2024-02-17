cbae77338a3a6e5c80cb4fbb6f284b4e701a6c79
Max <shadow.iqka@qq.com>
2024-02-17 19:01:06 +0800

    Add After function to time object

--- runtime/v8/objects/time/time_test.go
+++ runtime/v8/objects/time/time_test.go
@@ -2,7 +2,11 @@ package time
 
 import (
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/yaoapp/gou/process"
+	"github.com/yaoapp/kun/exception"
 	"rogchap.com/v8go"
 )
 
@@ -23,5 +27,62 @@ func TestSleep(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+}
+
+func TestAfter(t *testing.T) {
+	iso := v8go.NewIsolate()
+	defer iso.Dispose()
+
+	obj := &Object{}
+	ctx := v8go.NewContext(iso)
+	defer ctx.Close()
+	obj.Set("time", ctx)
+
+	testArgs := []interface{}{}
+
+	process.Register("unit.test.time", func(process *process.Process) interface{} {
+		testArgs = process.Args
+		return nil
+	})
+
+	_, err := ctx.RunScript(`
+		time.After(200, "unit.test.time", "foo", "bar")
+	`, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(300 * time.Millisecond)
+	assert.Equal(t, []interface{}{"foo", "bar"}, testArgs)
+
+}
+
+func TestAfterError(t *testing.T) {
+	iso := v8go.NewIsolate()
+	defer iso.Dispose()
+
+	obj := &Object{}
+	ctx := v8go.NewContext(iso)
+	defer ctx.Close()
+	obj.Set("time", ctx)
+
+	testArgs := []interface{}{}
+
+	process.Register("unit.test.time", func(process *process.Process) interface{} {
+		testArgs = process.Args
+		exception.New("unit.test.time", 200).Throw()
+		return nil
+	})
+
+	_, err := ctx.RunScript(`
+		time.After(200, "unit.test.time", "foo", "bar")
+	`, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(300 * time.Millisecond)
+	assert.Equal(t, []interface{}{"foo", "bar"}, testArgs)
 
 }
