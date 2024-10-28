126eb302a9ce0d55517e245c0c71d4b751c97739
Max <shadow.iqka@qq.com>
2024-10-28 20:23:57 +0800

    Upload process supports chunk uploading

--- fs/process.go
+++ fs/process.go
@@ -3,16 +3,16 @@ package fs
 import (
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/yaoapp/gou/process"
 	"github.com/yaoapp/gou/types"
 	"github.com/yaoapp/kun/exception"
 	"github.com/yaoapp/kun/log"
-	"github.com/yaoapp/kun/utils"
 )
 
 // FileSystemHandlers the file system handlers
@@ -474,29 +474,99 @@ func processUpload(process *process.Process) interface{} {
 		exception.New("parameters error: %v", 400, process.Args[0]).Throw()
 	}
 
-	fingerprint := strings.ToUpper(tmpfile.UID)
-	if fingerprint == "" {
-		fingerprint = strings.ToUpper(uuid.NewString())
-	}
+	uid := tmpfile.Hash()
 	dir := strings.Join([]string{string(os.PathSeparator), time.Now().Format("20060102")}, "")
 	ext := filepath.Ext(tmpfile.Name)
-	filename := filepath.Join(dir, fmt.Sprintf("%s%s", fingerprint, ext))
-
-	fmt.Println("-----------------, upload, ", filename, tmpfile.Range)
-	utils.Dump(tmpfile)
-	fmt.Println("-----------------, upload, ", filename, tmpfile.Range)
+	filename := filepath.Join(dir, fmt.Sprintf("%s%s", uid, ext))
 
 	stor := stor(process)
-	content, err := stor.ReadFile(tmpfile.TempFile)
-	if err != nil {
-		exception.New("unable to read uploaded file %s", 500, err.Error()).Throw()
+
+	// For chunk upload.
+	if tmpfile.IsChunk() {
+
+		// Sync upload, the chunk file will be merged directly.
+		if tmpfile.Sync {
+			err := MoveAppend(stor, tmpfile.TempFile, filename)
+			if err != nil {
+				exception.New(err.Error(), 500).Throw()
+			}
+
+			// Cheek the file is exists
+			size, err := Size(stor, filename)
+			if err != nil {
+				exception.New(err.Error(), 500).Throw()
+			}
+
+			total := tmpfile.TotalSize()
+			if int64(size) == total {
+				return filename
+			}
+
+			// Return the file path and the upload progress
+			progress := types.UploadProgress{
+				Total:     total,
+				Uploaded:  int64(size),
+				Completed: false,
+			}
+
+			return map[string]interface{}{
+				"path":     filename,
+				"uid":      tmpfile.UID,
+				"progress": progress,
+			}
+		}
+
+		// Async upload, the chunk file will be saved to the temp directory.
+		tmpDir := path.Join("upload", "tmp", uid)
+		err := stor.MkdirAll(tmpDir, uint32(os.ModePerm))
+		if err != nil {
+			exception.New(err.Error(), 500).Throw()
+		}
+
+		chunkFile := filepath.Join(tmpDir, tmpfile.ChunkFileName())
+		err = stor.Move(tmpfile.TempFile, chunkFile)
+		if err != nil {
+			exception.New(err.Error(), 500).Throw()
+		}
+
+		// Check if all chunks are uploaded.
+		progress, err := uploadProgress(stor, tmpDir)
+		if err != nil {
+			exception.New(err.Error(), 500).Throw()
+		}
+
+		// Remove the temp directory.
+		if progress.Completed {
+			defer stor.RemoveAll(tmpDir)
+
+			// Get Files
+			files, err := getChunkFiles(stor, tmpDir, true)
+			if err != nil {
+				exception.New(err.Error(), 500).Throw()
+			}
+
+			// Merge the chunk files.
+			for _, file := range files {
+				err := MoveAppend(stor, file, filename)
+				if err != nil {
+					exception.New(err.Error(), 500).Throw()
+				}
+			}
+			return filename
+		}
+
+		return map[string]interface{}{
+			"path":     filename,
+			"uid":      tmpfile.UID,
+			"progress": progress,
+		}
 	}
 
-	_, err = stor.WriteFile(filename, content, uint32(os.ModePerm))
+	// For normal upload.
+	err := stor.Move(tmpfile.TempFile, filename)
 	if err != nil {
-		exception.New("failed to save file %s", 500, err.Error()).Throw()
+		exception.New(err.Error(), 500).Throw()
 	}
-
 	return filename
 }
 
@@ -519,4 +589,91 @@ func processDownload(process *process.Process) interface{} {
 		"content": data,
 		"type":    mimeType,
 	}
+}
+
+func uploadProgress(stor FileSystem, path string) (types.UploadProgress, error) {
+	files, err := getChunkFiles(stor, path, true)
+	if err != nil {
+		return types.UploadProgress{}, err
+	}
+
+	// Check the chunk files
+	progress := types.UploadProgress{
+		Total:     0,
+		Uploaded:  0,
+		Completed: false,
+	}
+
+	for i, file := range files {
+		if i == 0 {
+			baseName := strings.ReplaceAll(file, ".chunk", "")
+			nameInfo := strings.Split(baseName, "_")
+			if len(nameInfo) != 2 {
+				return progress, fmt.Errorf("the chunk file %s is invalid", file)
+			}
+
+			// Get the chunk file size
+			var total int64
+			fmt.Sscanf(nameInfo[len(nameInfo)-1], "%d", &total)
+			progress.Total = total
+		}
+
+		// Get the chunk file size
+		size, err := Size(stor, file)
+		if err != nil {
+			return progress, err
+		}
+
+		progress.Uploaded += int64(size)
+	}
+
+	// Check the upload is completed or not
+	progress.Completed = progress.Uploaded == progress.Total
+	return progress, nil
+}
+
+func getChunkFiles(stor FileSystem, path string, sortable bool) ([]string, error) {
+
+	// Validate the path is exists
+	if _, err := stor.Exists(path); err != nil {
+		return nil, fmt.Errorf("the file %s is not exists", path)
+	}
+
+	// Validate the path is a directory
+	if !stor.IsDir(path) {
+		return nil, fmt.Errorf("the file %s is not a directory", path)
+	}
+
+	// Get the chunk files in the directory
+	files, err := stor.ReadDir(path, false)
+	if err != nil {
+		return nil, err
+	}
+
+	if !sortable {
+		return files, nil
+	}
+
+	chunkMap := map[string]int64{}
+	for _, file := range files {
+
+		// Get the chunk file name
+		baseName := strings.ReplaceAll(filepath.Base(file), ".chunk", "")
+		nameInfo := strings.Split(baseName, "-")
+		if len(nameInfo) != 2 {
+			continue
+		}
+
+		// Get the chunk file size
+		var from int64
+		fmt.Sscanf(nameInfo[0], "%d", &from)
+		chunkMap[file] = from
+	}
+
+	// Sort the chunk files
+	sort.Slice(files, func(i, j int) bool {
+		return chunkMap[files[i]] < chunkMap[files[j]]
+	})
+
+	return files, nil
 }
--- types/fs.go
+++ types/fs.go
@@ -1,15 +1,76 @@
 package types
 
-import "net/textproto"
+import (
+	"fmt"
+	"hash/fnv"
+	"net/textproto"
+	"strings"
+
+	"github.com/google/uuid"
+)
 
 // UploadFile upload file
 type UploadFile struct {
 	UID      string               `json:"uid,omitempty"`   // Content-Uid The unique identifier of the file ( for chunk upload )
 	Range    string               `json:"range,omitempty"` // Content-Range bytes start-end/total (for chunk upload)
-	Chunk    string               `json:"chunk,omitempty"` // Content-Chunk current/total (for chunk upload) ** this is not standard **
+	Sync     bool                 `json:"sync,omitempty"`  // Content-Sync sync upload or not. the default is false
 	Name     string               `json:"name"`
 	TempFile string               `json:"tempFile"`
 	Size     int64                `json:"size"`
 	Header   textproto.MIMEHeader `json:"header"`
 	Error    string               `json:"error"`
+}
+
+// UploadProgress upload progress
+type UploadProgress struct {
+	Total     int64 `json:"total"`
+	Uploaded  int64 `json:"uploaded"`
+	Completed bool  `json:"completed"`
+}
+
+// IsChunk is chunk upload or not
+func (upload UploadFile) IsChunk() bool {
+	return upload.Range != ""
+}
+
+// IsError is error or not
+func (upload UploadFile) IsError() bool {
+	return upload.Error != ""
+}
+
+// Hash hash
+func (upload UploadFile) Hash() string {
+
+	h := fnv.New64a()
+	// UUID Hash
+	if upload.UID != "" {
+		h.Write([]byte(fmt.Sprintf("%v", upload.UID)))
+		return fmt.Sprintf("%x", h.Sum64())
+	}
+
+	// TempFile Hash (for chunk upload)
+	if upload.TempFile != "" {
+		h.Write([]byte(fmt.Sprintf("%v", upload.TempFile)))
+		return fmt.Sprintf("%x", h.Sum64())
+	}
+
+	// General a uuid and hash
+	uuid := uuid.NewString()
+	h.Write([]byte(fmt.Sprintf("%v", uuid)))
+	return fmt.Sprintf("%x", h.Sum64())
+}
+
+// ChunkFileName get chunk file name
+func (upload UploadFile) ChunkFileName() string {
+	name := strings.ReplaceAll(upload.Range, "bytes ", "")
+	name = strings.ReplaceAll(name, "/", "_")
+	return fmt.Sprintf("%s.chunk", name)
+}
+
+// TotalSize total size
+func (upload UploadFile) TotalSize() int64 {
+	var total int64
+	nameInfo := strings.Split(upload.Range, "/")
+	fmt.Sscanf(nameInfo[len(nameInfo)-1], "%d", &total)
+	return total
 }
